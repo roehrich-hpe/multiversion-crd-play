6984d0b94ad1e9dd6954540e7bb7f0f1cccf790e
Dean Roehrich <dean.roehrich@hpe.com>
2023-03-30 13:47:18 -0500

    kubebuilder create api --group dws --version v1alpha1 --kind Vehicle --resource --controller

--- cmd/main.go
+++ cmd/main.go
@@ -100,6 +100,13 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "Desert")
 		os.Exit(1)
 	}
+	if err = (&controller.VehicleReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Vehicle")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
